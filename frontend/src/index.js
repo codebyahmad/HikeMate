a458be1b9c7a4b05423028c0544b16877f3688c1
codebyahmad <ahmadasad296@gmail.com>
2023-12-30 17:23:56 +0100

    resetting reactapp

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import * as serviceWorker from "./serviceWorker";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import App from "./App";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import reportWebVitals from "./reportWebVitals";
 
-const routing = (
-  <Router>
-    <React.StrictMode>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={App} />
-      </Switch>
-      <Footer />
-    </React.StrictMode>
-  </Router>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
