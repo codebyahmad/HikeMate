a478c827c40ed25a09a4f961345786bb3087c13b
codebyahmad <ahmadasad296@gmail.com>
2023-12-30 20:04:42 +0100

    frontend connected with backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,40 @@
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [trails, setTrails] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:8000/api/");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const jsonData = await response.json();
+        setTrails(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Your Fetched Data:</h1>
+        <ul>
+          {trails.map((item) => (
+            <li key={item.id}>
+              {/* Customize this part based on your data structure */}
+              <strong>{item.name}</strong>: {item.avg_rating}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
